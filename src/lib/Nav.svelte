2216b77f1968d3de0fdec3fdd570cb0a2f7d278f
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-10-12 14:33:50 +1300

    made the nav bar and did the styling

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,6 +1,7 @@
 <nav>
     <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-    </ul>
+        <li><a href="routes/+page.svelte">Home</a></li>
+        <li><a href="routes/about/+page.svelte">About</a></li>
+        <li><a href="routes/contact/+page.svelte">Contact</a></li>
+    </ul>  
 </nav>
