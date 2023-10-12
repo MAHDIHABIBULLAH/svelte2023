5b34edd66f282c2e3f2c14b2e7090fc67098ce94
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-10-12 22:47:46 +1300

    added css to nav bar

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,7 +2,15 @@
     import Nav from '$lib/Nav.svelte';
 </script>
 <header>
-    <Nav/>
-    Hello I am the header
- 
+    <div class="logo">
+        <img src="https://i.imgur.com/QKsDxyn.jpg"/>
+    </div>
+    <h1>ONE PIECE</h1>
 </header>
+<nav>
+    <ul>
+       <li><a href="routes/+page.svelte">Home</a></li>
+        <li><a href="routes/about/+page.svelte">About</a></li>
+        <li><a href="routes/contact/+page.svelte">Contact</a></li>
+    </ul>
+</nav>
