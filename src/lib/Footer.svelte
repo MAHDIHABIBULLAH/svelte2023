91570abeed14be3b53c443cd8de4b6da92f31ead
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-10-27 19:45:14 +1300

    added image to the footer page

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,4 +1,7 @@
 <footer>
+  <div class="footer-logo">
+    <img src="img/onepiecefooter.png">
+  </div>
     <div class="container">
         <p>&copy; 2023 One piece. All rights reserved.</p>
       </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,3 +4,4 @@
     <link rel="stylesheet" href="css/main.css"/>
 </svelte:head>
  <h1>hello</h1>
+ 
