542b9756ab78938167db713f7b18a448fd82a60b
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-10-28 12:32:57 +1300

    added the html code to emplement a video in my main page

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,5 +1,6 @@
 <nav>
     <img src="img/onepiece.png" width="200" alt="Icon place holder ">
+    <h1 class header>"I am gonna became the pirates king"</h1>
     <ul>
         <li><a href="src/routes/+page.svelte">Home</a></li>
         <li><a href="src/routes/about/+page.svelte">About</a></li>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,5 +3,10 @@
 <svelte:head>
     <link rel="stylesheet" href="css/main.css"/>
 </svelte:head>
- <h1>hello</h1>
+<div class="video-grid">
+    <div class="video">
+        <video src="videos/onepiece.mp4" type="video/mp4" alt="unable to play the video" controls></video>
+    </div>
+   
+
  
