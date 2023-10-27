0feb53ede40afa3aa7f650eee3189a8ef0a9a0b0
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-10-28 12:34:45 +1300

    added a small para about the anime one piece

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,12 @@
     <div class="video">
         <video src="videos/onepiece.mp4" type="video/mp4" alt="unable to play the video" controls></video>
     </div>
-   
+    <div class="details">
+        <h1>One Piece</h1>
+        <p>"One Piece" is a popular Japanese anime and manga series created by Eiichiro Oda. It follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, as they search for the ultimate treasure known as the "One Piece" to become the Pirate King.</p>
+        <p>The series is known for its rich world-building, diverse characters, epic battles, and themes of friendship, freedom, and the pursuit of dreams. It has captured the hearts of fans worldwide and has been a long-running success.</p>
+        <p>With a compelling storyline and a vast world to explore, "One Piece" has become one of the most beloved and enduring series in the anime and manga industry.</p>
+    </div>
+</div>
 
  
