d30ef31b8dd4977a6c4facec39c6e376cffaa993
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-10-28 19:44:01 +1300

    removed some useless code

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -8,6 +8,6 @@
       <div class="footer-links">
         <a href="https://www.instagram.com/tetsuya_kunn/" target="_blank"><img src="img/instagram.png" alt="Instagram"></a>
         <a href="https://www.snapchat.com/add/kenjiro_tsuda8" target="_blank"><img src="img/snapchat.png" alt="Snapchat"></a>
-        <a href="https://discord.com/invite/akiyno" target="_blank"><img src="img/discord.png" alt="Discord"></a>
+        <a href="https://discord.com/invite/akiyno#1230" target="_blank"><img src="img/discord.png" alt="Discord"></a>
       </div>
 </footer> 
