0d684090f02e07d84b89232bb978bdc2005281ab
MAHDIHABIBULLAH <HABIM2@student.op.ac.nz>
2023-10-30 15:59:06 +1300

    imported nav and footer to all the other pages and fixed the file paths for the nav bar buttons

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,11 +1,11 @@
-<nav>
+<nav class="nav">
     <img src="img/onepiece.png" width="200" alt="Icon place holder">
     <div class="navtext">
         <h1>"I am gonna become the pirates King"</h1>
     </div>
     <ul>
-        <li><a href="src/routes/+page.svelte">Home</a></li>
-        <li><a href="src/routes/Devilfruit/+page.svelte">Devil Fruit</a></li>
-        <li><a href="src/routes/contact/+page.svelte">Contact</a></li>
+        <li><a href="/">Home</a></li>
+        <li><a href="/Devilfruit">Devil Fruit</a></li>
+        <li><a href="/contact">Contact</a></li>
     </ul>
 </nav>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,9 +1,9 @@
-<h1 class="Header">Thank You for visiting my website</h1>
-<h5>For any recommendation or any suggestions you can reach out to me in :</h5>
-<button><img src="img/instagram.png"></button>
-<button></button>
-<button></button>
+<script>
+  import Nav from "$lib/Nav.svelte";
+  import Footer from "$lib/Footer.svelte";
+</script>
 
 <svelte:head>
-   <link rel="stylesheet" href="css/main.css" />
+  <link rel="stylesheet" href="css/main.css" />
 </svelte:head>
+
