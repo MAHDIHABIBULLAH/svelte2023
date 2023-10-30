a8ddeaf7050b13dcd6043eba8b2500dbe2e62ca8
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-10-30 20:54:17 +1300

    removed the java script from the main +page.svelte to other folder as I don't want to fetch api in my main page and want to maintaine it like that

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,3 @@
-<script>
-  import { onMount } from 'svelte';
-  
-let cardData = [];
-
-async function fetchCardData() {
-  try {
-    const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php');
-    const result = await response.json();
-    cardData = result.data;
-  } catch (error) {
-    console.error('Error fetching card data:', error);
-  }
-}
-onMount(fetchCardData);
-</script>
-{#if cardData.length > 0}
-  <ul>
-    {#each cardData as card (card.id)}
-      <li>
-        <img src={`https://images.ygoprodeck.com/images/cards/${card.id}.jpg`} alt={card.name} />
-        {card.name}
-      </li>
-    {/each}
-  </ul>
-{:else}
-  <p>No card data available.</p>
-{/if}
-
-
 <svelte:head>
     <link rel="stylesheet" href="css/main.css"/>
 </svelte:head>
---
+++ src/routes/Devilfruit/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+    import { onMount } from 'svelte';
+    
+  let cardData = [];
+  
+  async function fetchCardData() {
+    try {
+      const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php');
+      const result = await response.json();
+      cardData = result.data;
+    } catch (error) {
+      console.error('Error fetching card data:', error);
+    }
+  }
+  onMount(fetchCardData);
+  </script>
+  {#if cardData.length > 0}
+    <ul>
+      {#each cardData as card (card.id)}
+        <li>
+          <img src={`https://images.ygoprodeck.com/images/cards/${card.id}.jpg`} alt={card.name} />
+          {card.name}
+        </li>
+      {/each}
+    </ul>
+  {:else}
+    <p>No card data available.</p>
+  {/if}
+  
