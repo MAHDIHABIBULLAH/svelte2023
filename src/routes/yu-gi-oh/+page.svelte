80a2b462d735c292dfed2d04f1d8139bc5792b0f
MAHDIHABIBULLAH <HABIM2@student.op.ac.nz>
2023-10-31 13:37:29 +1300

    added the classes in my printing out the api results so I can style them in grid and give them havour effect

--- src/routes/yu-gi-oh/+page.svelte
+++ src/routes/yu-gi-oh/+page.svelte
@@ -15,15 +15,14 @@
   onMount(fetchCardData);
   </script>
   {#if cardData.length > 0}
-    <ul>
-      {#each cardData as card (card.id)}
-        <li>
-          <img src={`https://images.ygoprodeck.com/images/cards/${card.id}.jpg`} alt={card.name} />
-          {card.name}
-        </li>
-      {/each}
-    </ul>
-  {:else}
-    <p>No card data available.</p>
-  {/if}
-  
+  <ul class="card-grid">
+    {#each cardData as card (card.id)}
+      <li class="card">
+        <img src={`https://images.ygoprodeck.com/images/cards/${card.id}.jpg`} alt={card.name} />
+        <p>{card.name}</p>
+      </li>
+    {/each}
+  </ul>
+{:else}
+  <p>No card data available.</p>
+{/if}
