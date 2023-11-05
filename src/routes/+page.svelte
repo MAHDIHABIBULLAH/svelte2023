0a41413c669b00ecf505777229a7a37634d40165
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-11-05 17:42:57 +1300

    added the java script to display the wanted poster in my main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,17 @@
+<script>
+let imagePaths = [
+    "img/1.jpg",
+    "img/2.jpg",
+    "img/3.jpg",
+    "img/4.jpg",
+    "img/5.jpg",
+    "img/6.jpg",
+    "img/7.jpg",
+    "img/8.jpg",
+    "img/9.jpg",
+    "img/10.jpg",
+  ];
+</script>
 <svelte:head>
     <link rel="stylesheet" href="css/main.css"/>
 </svelte:head>
