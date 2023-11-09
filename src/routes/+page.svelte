afb5494ef301ea7235289412c636824073acbced
Mahdi Habibullah <HABIM2@student.op.ac.nz>
2023-11-09 19:40:16 +1300

    removed unnecessary elements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,15 @@
 <div class="video-grid">
     <div class="video">
-        <video src="videos/onepiece.mp4" type="video/mp4" alt="unable to play the video" controls></video>
+      <video src="videos/onepiece.mp4" type="video/mp4" alt="Unable to play the video" controls></video>
     </div>
     <div class="details">
-        <h1>One Piece</h1>
-        <p>"One Piece" is a popular Japanese anime and manga series created by Eiichiro Oda. It follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, as they search for the ultimate treasure known as the "One Piece" to become the Pirate King.
-        The series is known for its rich world-building, diverse characters, epic battles, and themes of friendship, freedom, and the pursuit of dreams. It has captured the hearts of fans worldwide and has been a long-running success.
-        With a compelling storyline and a vast world to explore, "One Piece" has become one of the most beloved and enduring series in the anime and manga industry.</p>
-        <p>One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since July 1997, with its individual chapters compiled into 106 tankōbon volumes as of July 2023. The story follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, where he explores the Grand Line in search of the mythical treasure known as the "One Piece" in order to become the next King of the Pirates.</p>
-        
+      <h1>One Piece</h1>
+      <p>
+        "One Piece" is a popular Japanese anime and manga series created by Eiichiro Oda. It follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, as they search for the ultimate treasure known as the "One Piece" to become the Pirate King. The series is known for its rich world-building, diverse characters, epic battles, and themes of friendship, freedom, and the pursuit of dreams. It has captured the hearts of fans worldwide and has been a long-running success.
+      </p>
+      <p>
+        One Piece is a Japanese manga series written and illustrated by Eiichiro Oda. It has been serialized in Shueisha's shōnen manga magazine Weekly Shōnen Jump since July 1997, with its individual chapters compiled into 106 tankōbon volumes as of July 2023. The story follows the adventures of Monkey D. Luffy and his crew, the Straw Hat Pirates, where he explores the Grand Line in search of the mythical treasure known as the "One Piece" in order to become the next King of the Pirates.
+      </p>
     </div>
-</div>
+  </div>
+  
