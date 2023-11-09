e855869b510d258ccb333e8c263ea590ff481c8a
MAHDIHABIBULLAH <HABIM2@student.op.ac.nz>
2023-11-09 15:13:28 +1300

    removed the svelte head tag since its already there in layout.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<svelte:head>
-    <link rel="stylesheet" href="css/main.css"/>
-</svelte:head>
 <div class="video-grid">
     <div class="video">
         <video src="videos/onepiece.mp4" type="video/mp4" alt="unable to play the video" controls></video>
@@ -14,5 +11,3 @@
         
     </div>
 </div>
-
- 
