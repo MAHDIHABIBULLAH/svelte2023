aec697cd4ba363a7ccd969620c28bb8baeb7e0f4
MAHDIHABIBULLAH <HABIM2@student.op.ac.nz>
2023-11-10 18:29:00 +1300

    changed the media styling so that when the website is minimized it shrinks and fits with it the buttons and everthing

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,111 +1,105 @@
 <script>
-    let currentYear = new Date().getFullYear();
-  
-    const navLinks = [
-      { href: '/', text: 'One Piece' },
-      { href: '/yu-gi-oh', text: 'Yu-gi-oh' },
-      { href: '/contact', text: 'Contact' },
-      {href: '/pictures', text:'Pictures'}
-    ];
-  </script>
-  
-  <nav class="nav">
-    <a href="/"><img src="img/onepiece.png" width="200" alt="One Piece Icon"></a>
-    <div class="yugi-logo">
-   <a href="/yu-gi-oh"><img src="img/yugi.png" width="200" alt="Yu-gi-oh Icon"></a>
-    </div>
-    <ul>
-      {#each navLinks as { href, text }}
-        <li><a href={href}>{text}</a></li>
-      {/each}
-    </ul>
-  </nav>
-  
-  <style>
+  const navLinks = [
+    { href: '/', text: 'One Piece' },
+    { href: '/yu-gi-oh', text: 'Yu-gi-oh' },
+    { href: '/contact', text: 'Contact' },
+    { href: '/pictures', text: 'Pictures' }
+  ];
+</script>
 
+<nav class="nav">
+  <a href="/" class="nav-icon"><img src="img/onepiece.png" alt="One Piece Icon"></a>
+  <div class="yugi-logo">
+    <a href="/yu-gi-oh" class="nav-icon"><img src="img/yugi.png" alt="Yu-gi-oh Icon"></a>
+  </div>
+  <ul class="nav-list">
+    {#each navLinks as { href, text }}
+      <li class="nav-item"><a href={href} class="nav-link">{text}</a></li>
+    {/each}
+  </ul>
+</nav>
 
-/* Navigation styling */
-.nav {
-  margin: 0;
-  background-color: #333;
-  color: #fff;
-  position: relative;
-  width: 100%;
-}
+<style>
+  /* Base styles */
+  body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden; /* Prevents horizontal scroll */
+  }
 
-.yugi-logo {
-  position: absolute;
-  top: 0;
-  right: 0;
-}
-
-.nav ul {
-  list-style-type: none;
-  padding: 0;
-  margin: 0;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  height: 50px;
-}
-
-nav li {
-  text-align: center;
-}
-
-nav a {
-  text-decoration: none;
-  background-color: rgb(51, 51, 51);
-  color: #fff4fe;
-  padding: 10px 20px;
-  transition: background-color 0.3s;
-}
-
-nav a:hover {
-  background-color: #d8d8d8;
-}
- /* Add some padding to the nav */
- .nav {
+  /* Navigation styling */
+  .nav {
+    background-color: #333;
+    color: #fff;
     padding: 10px;
+    width: 100%;
+    box-sizing: border-box; /* Includes padding in the element's total width */
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap; /* Ensures nav content wraps on smaller screens */
   }
 
-  
-  /* Styling the navigation links */
-  nav a {
+  .nav-icon img {
+    width: 100%;
+    max-width: 200px; /* Limits image size */
+    height: auto;
+  }
+
+  .yugi-logo {
+    margin-left: auto; /* Pushes the logo to the right */
+  }
+
+  .nav-list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    width: 100%; /* Full width to contain its children */
+    justify-content: space-around;
+    flex-wrap: wrap; /* Allow list items to wrap */
+  }
+
+  .nav-item {
+    padding: 10px;
+    box-sizing: border-box;
+  }
+
+  .nav-link {
+    text-decoration: none;
+    color: #fff4fe;
+    padding: 10px 20px;
     border-radius: 5px;
-    margin: 0 5px;
+    transition: background-color 0.3s, box-shadow 0.3s;
   }
 
-  /* Style the active link */
-  nav a.active {
+  .nav-link:hover {
     background-color: #d8d8d8;
     color: #333;
-  }
-
-  /* Add a subtle box shadow on hover */
-  nav a:hover {
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
   }
 
-  /* Media query for responsive design */
+  /* Responsive design adjustments */
   @media (max-width: 600px) {
     .nav {
       flex-direction: column;
-      align-items: flex-start;
+      align-items: center;
     }
 
-    .yugi-logo {
-      position: static;
-      margin-bottom: 10px;
+    .yugi-logo, .nav-icon {
+      width: 100%; /* Full width to center the images */
+      display: flex;
+      justify-content: center;
     }
 
-    nav ul {
+    .nav-list {
       flex-direction: column;
+      align-items: center;
     }
 
-    nav li {
-      margin-bottom: 5px;
+    .nav-link {
+      width: 100%; /* Full width links on mobile */
+      margin: 5px 0; /* Space out links vertically */
+      text-align: center;
     }
   }
-
-  </style>
+</style>
