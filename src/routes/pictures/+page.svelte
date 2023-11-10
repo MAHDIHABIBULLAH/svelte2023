9306b7e802d166bd447e9fc7784471d4c7057a5f
MAHDIHABIBULLAH <HABIM2@student.op.ac.nz>
2023-11-10 18:02:35 +1300

    added annimation on the h1 tag and hovering effect

--- src/routes/pictures/+page.svelte
+++ src/routes/pictures/+page.svelte
@@ -13,5 +13,30 @@
       margin: auto;
       text-align: center;
     }
+  
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+  
+    h1 {
+      font-family: 'Arial', sans-serif;
+      color: #333;
+      margin: 0;
+      padding: 20px 0;
+      font-size: 2.5rem;
+      text-transform: uppercase;
+      letter-spacing: 1px;
+      animation: fadeIn 1s ease-out forwards;
+      transition: color 0.3s ease; /* Transition for the color change on hover */
+    }
+  
+    h1:hover {
+      color: #555; /* Darker shade when hovered */
+    }
   </style>
   
