14d9cf17e29e8c85636a531060258ba39ddafcb1
MAHDIHABIBULLAH <HABIM2@student.op.ac.nz>
2023-11-10 17:50:40 +1300

    added the pictures and the javascript for the picture components

---
+++ src/lib/PictureGallery.svelte
@@ -0,0 +1,25 @@
+<script>
+    let images = [
+      "1.jpg",
+      "2.jpg",
+      "3.jpg",
+      "4.jpg",
+      "5.jpg",
+      "6.jpg",
+      "7.jpg",
+      "8.jpg",
+      "9.jpg",
+      "10.jpg"
+    ];
+  
+    let zoomedImage = null;
+    function zoomImage(event, img) {
+      zoomedImage = img;
+      event.stopPropagation(); // Prevent the event from bubbling up to the window
+    }
+  
+    function resetZoom() {
+      zoomedImage = null;
+    }
+    window.addEventListener('click', resetZoom);
+  </script>
