a52831304a8c25094460987a17eabad2ef6ee1b0
MAHDIHABIBULLAH <HABIM2@student.op.ac.nz>
2023-11-10 17:54:27 +1300

    imported the component PictureGallery.svelte to Pictures +page.svelte and displayed the contents

---
+++ src/routes/pictures/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import PictureGallery from "$lib/PictureGallery.svelte";
+  </script>
+  
+  <main>
+    <h1>Picture Gallery</h1>
+    <PictureGallery />
+  </main>
+  
+  <style>
+    main {
+      max-width: 60%;
+      margin: auto;
+      text-align: center;
+    }
+  </style>
+  
